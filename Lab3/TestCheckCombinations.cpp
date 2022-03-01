890f4ccf181ee0eb0d01fd158202da62beae54c1
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-03-01 09:46:24 +0300

    Lab3: исправил тест проверки комбинаций

--- Lab3/TestCheckCombinations.cpp
+++ Lab3/TestCheckCombinations.cpp
@@ -1,31 +1,32 @@
 #include "TestCheckCombinations.h"
 
-std::vector<std::vector<uint16_t>> getSubsequences(std::string bitSequence, uint16_t t)
+std::vector<std::string> getSubsequences(std::string bitSequence, uint16_t t)
 {
-	std::vector<std::vector<uint16_t>> result;
+	std::vector<std::string> result;
 	for (size_t i = 0; i != bitSequence.size(); i += t)
 	{
 		std::string sub = bitSequence.substr(i, t);
 		if (sub.size() != t) break;
+		result.push_back(sub);
 	}
 	return result;
 }
 std::map<uint16_t, uint16_t> getLengOfSubsequences(std::string bitSequence, uint16_t t)
 {
 	std::map<uint16_t, uint16_t> result;
-	std::vector<std::vector<uint16_t>> subsequences = getSubsequences(bitSequence, t);
+	std::vector<std::string> subSequences = getSubsequences(bitSequence, t);
 
 	for (size_t i = 1; i <= 2; i++) result[i] = 0;
 
 	uint16_t counterOne = 0;
 	uint16_t counterZero = 0;
 
-	for (const auto& v : subsequences)
+	for (const auto& v : subSequences)
 	{
 		for (const auto& num : v)
 		{
-			if (num == 1) counterOne++;
-			if (num == 0) counterZero++;
+			if ((num - '0') == 1) counterOne++;
+			if ((num - '0') == 0) counterZero++;
 		}
 		if ((counterOne != 0) and (counterZero != 0)) result[2]++;
 		if ((counterOne == 0) or (counterZero == 0)) result[1]++;
