0231083d95950a142278a05236a002d57d889ae2
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-03-20 23:24:32 +0300

    Исправлен тест линейной сложности. Теперь он работает с любой длинной бит

--- Lab3/TestCheackLinearComplexityProfile.cpp
+++ Lab3/TestCheackLinearComplexityProfile.cpp
@@ -2,46 +2,46 @@
 
 void StartCheackLinearComplexityProfile(std::string key)
 {
-	const int16_t max_N = 100;
-	int16_t s[max_N], c[max_N], b[max_N], t[max_N], buf[max_N], l, m, d;
+	int64_t l, m, d = 0;
+	std::vector<int16_t> s, c, b, t, buf;
 	size_t N = key.size();
 
-	for (int16_t i = 0; i < N; i++)
+	for (size_t i = 0; i < N; i++)
 	{
-		if (key[i] == '1') s[i] = 1;
-		else s[i] = 0;
+		s.push_back((key[i] == '1') ? 1 : 0);
 	}
 
-	for (int16_t i = 0; i <= N; i++)
+	for (size_t i = 0; i <= N; i++)
 	{
-		b[i] = 0;
-		c[i] = 0;
-		t[i] = 0;
+		b.push_back(0);
+		c.push_back(0);
+		t.push_back(0);
 	}
 	b[0] = c[0] = 1;
 	l = 0;
 	m = -1;
 	std::cout << "StartCheackLinearComplexityProfile: " << std::endl;
-	for (int16_t n = 0; n < N; n++)
+	for (size_t n = 0; n < N; n++)
 	{
 		d = s[n];
-		for (int16_t i = 1; i <= l; i++) d += (c[i] * s[n - i]);
+		for (size_t i = 1; i <= l; i++) d += (c[i] * s[n - i]);
 		d = d % 2;
-		for (int16_t i = 0; i < N; i++) buf[i] = 0;
+		buf.clear();
+		for (size_t i = 0; i < N; i++) buf.push_back(0);
 		if (d == 1)
 		{
-			for (int16_t i = 0; i <= l; i++) t[i] = c[i];
-			for (int16_t i = 0; i <= n - m; i++) buf[i + n - m] = b[i];
-			for (int16_t i = 0; i < N; i++) c[i] = (c[i] + buf[i]) % 2;
+			for (size_t i = 0; i <= l; i++) t[i] = c[i];
+			for (size_t i = 0; i <= n - m; i++) buf[i + n - m] = b[i];
+			for (size_t i = 0; i < N; i++) c[i] = (c[i] + buf[i]) % 2;
 			if (l <= n / 2)
 			{
 				l = n + 1 - l;
 				m = n;
-				for (int16_t i = 0; i <= l; i++) b[i] = t[i];
+				for (size_t i = 0; i <= l; i++) b[i] = t[i];
 			}
 		}
 		std::cout << "\tL= " << l << "\t Sn: ";
-		for (int16_t i = 0; i <= n; i++)
+		for (size_t i = 0; i <= n; i++)
 		{
 			std::cout << s[i];
 			if (i != n) std::cout << ", ";
--- Lab3/TestCheackLinearComplexityProfile.h
+++ Lab3/TestCheackLinearComplexityProfile.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
+
 
 void StartCheackLinearComplexityProfile(std::string key);
